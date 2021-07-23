332c54d5d8b8a81530d3d4386474e2c3dd913bb5
Robert Detjens <github@detjens.dev>
2021-07-23 16:55:20 -0700

    improve example role command
    Signed-off-by: Robert Detjens <github@detjens.dev>

--- modules/general.rb
+++ modules/general.rb
@@ -101,7 +101,7 @@ module GeneralCommands
     else # list roles if no action given
       event.channel.send_embed do |embed|
         embed.fields = [
-          { name: 'Usage:', value: "`!role add role [role2 ...]`\n`!role remove role [role2 ...]`" },
+          { name: 'Usage:', value: "`!role add foo [bar baz ...]`\n`!role remove foo [bar baz ...]`" },
           { name: 'Valid roles:', value: "`#{CONFIG['class_roles'].keys.map { |k| k.ljust 7 }.join('` `')}`" },
           { name: 'Missing a class?', value: 'If we are missing a class, let us know and we will add a channel!' }
         ]
