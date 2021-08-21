4fef30fa60d767d0a67a5c12e565fde36a9773bc
Robert Detjens <github@detjens.dev>
2021-08-20 18:38:27 -0700

    appease rubocop
    Signed-off-by: Robert Detjens <github@detjens.dev>

--- modules/slashcommands.rb
+++ modules/slashcommands.rb
@@ -34,38 +34,38 @@ module SlashCommands
       end
 
       # since max of 25 choices per dropdown, break up by level
-      components.row do |r|
+      components.row do |row|
         # general roles (not class)
-        roles = ROLES.filter { |n, _| n.match?(/^\D+$/) }
-        r.select_menu(custom_id: 'role_add_general', placeholder: 'General roles', max_values: roles.size) do |s|
-         roles.each do |role, id|
+        r = ROLES.filter { |n, _| n.match?(/^\D+$/) }
+        row.select_menu(custom_id: 'role_add_general', placeholder: 'General roles', max_values: r.size) do |s|
+          r.each do |role, id|
             s.option(label: role.capitalize, value: id.to_s)
           end
         end
       end
-      components.row do |r|
+      components.row do |row|
         # 100/200 level
-        roles = ROLES.filter { |n, _| n.match?(/[12]\d\d/) }
-        r.select_menu(custom_id: 'role_add_100/200', placeholder: '100/200-level classes', max_values: roles.size) do |s|
-          roles.each do |role, id|
+        r = ROLES.filter { |n, _| n.match?(/[12]\d\d/) }
+        row.select_menu(custom_id: 'role_add_100/200', placeholder: '100/200-level classes', max_values: r.size) do |s|
+          r.each do |role, id|
             s.option(label: role.upcase, value: id.to_s)
           end
         end
       end
-      components.row do |r|
+      components.row do |row|
         # 300 level
-        roles = ROLES.filter { |n, _| n.match?(/3\d\d/) }
-        r.select_menu(custom_id: 'role_add_300', placeholder: '300-level classes', max_values: roles.size) do |s|
-          roles.each do |role, id|
+        r = ROLES.filter { |n, _| n.match?(/3\d\d/) }
+        row.select_menu(custom_id: 'role_add_300', placeholder: '300-level classes', max_values: r.size) do |s|
+          r.each do |role, id|
             s.option(label: role.upcase, value: id.to_s)
           end
         end
       end
-      components.row do |r|
+      components.row do |row|
         # 400 level
-        roles = ROLES.filter { |n, _| n.match?(/4\d\d/) }
-        r.select_menu(custom_id: 'role_add_400', placeholder: '400-level classes', max_values: roles.size) do |s|
-          roles.each do |role, id|
+        r = ROLES.filter { |n, _| n.match?(/4\d\d/) }
+        row.select_menu(custom_id: 'role_add_400', placeholder: '400-level classes', max_values: r.size) do |s|
+          r.each do |role, id|
             s.option(label: role.upcase, value: id.to_s)
           end
         end
@@ -95,7 +95,7 @@ module SlashCommands
   application_command(:role).subcommand(:remove) do |event|
     event.respond(ephemeral: true) do |builder, components|
       # figure out what class roles the user has
-      common_roles = event.user.roles.map(&:id) & ROLES.values
+      common_r = event.user.roles.map(&:id) & ROLES.values
 
       if common_roles.empty?
         builder.add_embed do |embed|
@@ -114,9 +114,9 @@ module SlashCommands
         embed.color = CONFIG['colors']['info']
       end
 
-      components.row do |r|
-        r.select_menu(custom_id: 'role_remove', placeholder: 'Select roles!', max_values: common_roles.size) do |s|
-          common_roles.each do |id|
+      components.row do |row|
+        row.select_menu(custom_id: 'role_remove', placeholder: 'Select roles!', max_values: common_r.size) do |s|
+          common_r.each do |id|
             s.option(label: ROLES.key(id).upcase, value: id.to_s)
           end
         end
