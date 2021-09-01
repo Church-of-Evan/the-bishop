efe40e71ed9a4efa933558aa1d6f07bed93e1535
Robert Detjens <github@detjens.dev>
2021-09-01 16:08:07 -0700

    add non-ephemeral role selection command
    Signed-off-by: Robert Detjens <github@detjens.dev>

--- modules/admin.rb
+++ modules/admin.rb
@@ -93,4 +93,52 @@ module AdminCommands
       embed.color = CONFIG['colors']['success']
     end
   end
+
+  command(:role_message, required_roles: [CONFIG['roles']['admin']]) do |event|
+    event.send_embed do |embed, components|
+      embed.fields = [
+        { name: 'Role Selection', value: "Select roles in the dropdowns below:\n(sorted by class number)" }
+      ]
+      embed.color = CONFIG['colors']['info']
+
+      # since max of 25 choices per dropdown, break up by level
+      components.row do |row|
+        # general roles (not class)
+        r = ROLES.filter { |n, _| n.match?(/^\D+$/) }
+        row.select_menu(custom_id: 'role_add_general', placeholder: 'General roles', max_values: r.size) do |s|
+          r.each do |role, id|
+            s.option(label: role.capitalize, value: id.to_s)
+          end
+        end
+      end
+      components.row do |row|
+        # 100/200 level
+        r = ROLES.filter { |n, _| n.match?(/[12]\d\d/) }
+        row.select_menu(custom_id: 'role_add_100/200', placeholder: '100/200-level classes',
+                        max_values: r.size) do |s|
+          r.each do |role, id|
+            s.option(label: role.upcase, value: id.to_s)
+          end
+        end
+      end
+      components.row do |row|
+        # 300 level
+        r = ROLES.filter { |n, _| n.match?(/3\d\d/) }
+        row.select_menu(custom_id: 'role_add_300', placeholder: '300-level classes', max_values: r.size) do |s|
+          r.each do |role, id|
+            s.option(label: role.upcase, value: id.to_s)
+          end
+        end
+      end
+      components.row do |row|
+        # 400 level
+        r = ROLES.filter { |n, _| n.match?(/4\d\d/) }
+        row.select_menu(custom_id: 'role_add_400', placeholder: '400-level classes', max_values: r.size) do |s|
+          r.each do |role, id|
+            s.option(label: role.upcase, value: id.to_s)
+          end
+        end
+      end
+    end
+  end
 end
